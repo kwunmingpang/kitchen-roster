e54e2f7df83f077a8f180aaed6060156c7f6252b
kwunmingpang <pangkwunming@gmail.com>
2024-02-07 14:19:53 +1100

    build

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { differenceInWeeks } from "date-fns";
+import { useEffect, useState } from "react";
+
+// const projectDate = new Date("2024-01-01");
+const projectDate = new Date("2024-02-07");
+const rosterTable = ["luan", "peter", "zhizen", "naran"];
+const offset = 3;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [duty, setDuty] = useState("")
+
+    useEffect(() => {
+        const update = () => {
+            let weeks = differenceInWeeks(new Date(), projectDate);
+            let selection = (weeks + offset) % 4;
+            console.log("current roster", rosterTable[selection]);
+            setDuty(rosterTable[selection])
+        }
+        update()
+        setInterval(update, 60000)
+    }, []);
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <div className="container">
+                    <div>
+                        <h4>The kitchen duty this week is</h4>
+                        <h3 className="duty-header">{duty}</h3>
+                        <div className="notice">
+                            Please:
+                            <ul>
+                                <li>
+                                    take out the <b>trash</b> when it is <em>full</em> and by the <em>end of the week</em>
+                                </li>
+                                <li>
+                                    clean the <b>stovetop</b> at least <em>twice</em> per week
+                                </li>
+                                <li>
+                                    clean the <b>kitchen couter</b> at least <em>twice</em> per week
+                                </li>
+                                <li>
+                                    clean the <b>sink</b> at least <em>twice</em> per week
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </header>
+        </div>
+    );
 }
 
 export default App;
